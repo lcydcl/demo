685c82195b31770f077d41ac8823b6ba22e540b5
lcydcl <675989124@qq.com>
2016-12-22 02:05:12 +0800

    11222333

---
+++ web/controller/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/22
+ * Time: 2:03
+ */
+class Index
+{
+    public function show()
+    {
+        echo "123";
+    }
+}
